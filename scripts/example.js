03530d04d3e409194a2c338b531f3f1d2bf97ce1
brightboost <info@brightboost.nl>
2022-08-16 10:46:27 +0200

    js stuff

--- scripts/example.js
+++ scripts/example.js
@@ -13,13 +13,39 @@ function magic() {
 
   if (text == "no") {
     console.log("blablabla");
+  } else {
+    console.log("else statement");
   }
 
   // arrays
+  let arr = [4, "strings", true];
+  console.log(arr[0]);
+  console.log(arr.length);
+
+  for (let i = 0; i < arr.length; i++) {
+    console.log("loop:", arr[i]);
+  }
+
+  arr.shift(); //remove first
+  arr.push("something"); // append
+  console.log(arr);
 
   // objects
+  let person = {
+    name: "Jinthar",
+    hobby: ["angular", "python", "jigsaw puzzles"],
+    pet: {
+      type: "german shephard",
+      name: "ryan",
+    },
+  };
+
+  let x = "hobby";
+  console.log(person.name);
+  console.log(person[x]);
+
+  console.log(person.hobby[2]);
+  console.log(person.pet.name);
 
   // interacting with the DOM
-
-  console.log("hello from up here");
 }
