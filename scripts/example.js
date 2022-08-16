1ad3f358ae1cf754f1d069582c9251d368599c9e
brightboost <info@brightboost.nl>
2022-08-16 10:34:31 +0200

    more js

---
+++ scripts/example.js
@@ -0,0 +1,25 @@
+function magic() {
+  // variables
+  let nr = 5.6;
+  console.log(nr);
+
+  let nr1 = 3.2;
+  console.log((nr * nr1).toFixed(2));
+
+  console.log(Math.sqrt(nr));
+
+  let text = "Hello JS!";
+  let b = true;
+
+  if (text == "no") {
+    console.log("blablabla");
+  }
+
+  // arrays
+
+  // objects
+
+  // interacting with the DOM
+
+  console.log("hello from up here");
+}
